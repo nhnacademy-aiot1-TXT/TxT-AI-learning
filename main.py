db52150d232c4b055ff3f5fdb51f5cc59656b559
{GaHyoung} <[mnb2457@naver.com](mailto:mnb2457@naver.com)>
2024-04-23 10:22:35 +0900

    Feat: Scheduler 기능 추가

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ from model_manager import ModelManager
 from data_manager import DataManager
 from issue_token import get_token
 import pandas as pd
+import schedule
 import joblib
+import time
 
 # 데이터 로드 및 데이터프레임으로 변환
 def prepare_data(data_manager, topics):
@@ -68,6 +70,13 @@ def main():
     model = train_and_evaluate_models(data_df_filled)
     save_and_upload_model(model, env_vars)
 
+def schedule_main():
+    schedule.every(10).minutes.do(main)
+
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+
 
 if __name__ == '__main__':
-    main()
+    schedule_main()
